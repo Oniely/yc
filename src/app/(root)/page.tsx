2e8c8aadedf11a2624da4c81c4cb407269af2b2e
Oniely <jabagatniel09@gmail.com>
2025-01-20 18:49:45 +0800

    yc

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,58 @@
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+
+export default async function Home({
+	searchParams,
+}: {
+	searchParams: Promise<{ query?: string }>;
+}) {
+	const query = (await searchParams).query;
+
+	const posts = [
+		{
+			_id: 1,
+			_createdAt: new Date(),
+			views: 55,
+			author: {
+				_id: 1,
+				name: "Oniely",
+			},
+			description: "This is a description",
+			image: "https://images.unsplash.com/photo-1737265396678-a7dac6fcdff2?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D",
+			category: "Robots",
+			title: "We Robots",
+		},
+	];
+
+	return (
+		<>
+			<section className="pink_container">
+				<h1 className="heading">
+					Pitch Your Startup, <br />
+					Connect With Entrepreneurs
+				</h1>
+				<p className="sub-heading !max-w-3xl">
+					Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+					Competitions.
+				</p>
+				<SearchForm query={query} />
+			</section>
+
+			<section className="section_container">
+				<p className="text-30-semibold">
+					{query ? `Search results for "${query}"` : "All Startups"}
+				</p>
+
+				<ul className="mt-7 card_grid">
+					{posts.length > 0 ? (
+						posts.map((post, idx) => (
+							<StartupCard key={post._id} post={post} />
+						))
+					) : (
+						<p className="no-results">No startups found</p>
+					)}
+				</ul>
+			</section>
+		</>
+	);
+}
